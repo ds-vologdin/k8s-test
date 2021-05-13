584a2e556db544be3b9c9862e3d281a3364ff8a6
Dmitry Vologdin <dsvologo@mts.ru>
2021-05-13 12:02:15 +0300

    service for working with users

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ds-vologdin/k8s-test
 
 go 1.16
 
-require github.com/prometheus/client_golang v1.10.0
+require (
+	github.com/go-pg/pg/v10 v10.9.1
+	github.com/prometheus/client_golang v1.10.0
+)
--- main.go
+++ main.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/go-pg/pg/v10/orm"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/go-pg/pg/v10"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
 	maxShutdownTime = 10 * time.Second
+
+	countFakeUsers = 10000
 )
 
 var (
 	port = flag.Uint("port", 8000, "server port")
+	dsn  = flag.String("dsn", "postgresql://app:Hid8ujik@app-postgresql/app?sslmode=disable", "postgres dsn")
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
@@ -30,23 +37,31 @@ func health(w http.ResponseWriter, r *http.Request) {
 func root(w http.ResponseWriter, r *http.Request) {
 	log.Printf("[ROOT HANDLER] request: %v %v", r.Method, r.URL)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("root"))
+	w.Write([]byte("root handler\n"))
 }
 
-func newServer(address string) *http.Server {
+func newServer(address string, svc *Service) *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz/", health)
 	mux.HandleFunc("/", root)
+	mux.HandleFunc("/user/count", svc.GetCountUsers)
+	mux.HandleFunc("/user/random", svc.GetRandomUser)
 	mux.Handle("/metrics", promhttp.Handler())
 	return &http.Server{Addr: address, Handler: mux}
 }
 
 func main() {
+	rand.Seed(time.Now().Unix())
+
 	flag.Parse()
 	address := fmt.Sprintf(":%d", *port)
-	log.Printf("Start HTTP server %s", address)
 
-	srv := newServer(address)
+	svc, err := NewService(*dsn)
+	if err != nil {
+		log.Printf("create service: %v", err)
+		return
+	}
+	srv := newServer(address, svc)
 
 	shutdown := make(chan struct{})
 	defer close(shutdown)
@@ -68,10 +83,117 @@ func main() {
 		shutdown <- struct{}{}
 	}()
 
+	log.Printf("Start HTTP server %s", address)
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
 
 	log.Print("Wait for the shutdown server")
 	<-shutdown
+}
+
+// Service
+
+type Service struct {
+	db *pg.DB
+}
+
+func (s *Service) GetCountUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	if err := s.db.Model(&users).Select(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("{count of user: %d}", len(users))))
+}
+
+func (s *Service) GetRandomUser(w http.ResponseWriter, r *http.Request) {
+	id := rand.Int63n(countFakeUsers)
+	userName := fmt.Sprintf("fake-%d", id)
+	log.Printf("get user %s", userName)
+	user := User{}
+	err := s.db.Model(&user).
+		Where("name = ?", userName).
+		Limit(1).
+		Select()
+	if err != nil {
+		log.Printf("get random user (id: %d): %v", id, err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	body, err := json.Marshal(user)
+	if err != nil {
+		log.Printf("marshal user: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("internal error"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func NewService(dsn string) (*Service, error) {
+
+	opt, err := pg.ParseURL(dsn)
+	if err != nil {
+		return nil, err
+	}
+	db := pg.Connect(opt)
+	err = createSchema(db)
+	if err != nil {
+		log.Printf("create schema: %v", err)
+	} else {
+		log.Printf("create schema: done")
+	}
+	err = createFakeUsers(db, countFakeUsers)
+	if err != nil {
+		log.Printf("create fake users: %v", err)
+	}
+	return &Service{db: db}, nil
+}
+
+// Database
+
+type User struct {
+	Id     int64
+	Name   string
+	Emails []string
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("User<%d %s %v>", u.Id, u.Name, u.Emails)
+}
+
+func createSchema(db *pg.DB) error {
+	models := []interface{}{
+		(*User)(nil),
+	}
+
+	for _, model := range models {
+		err := db.Model(model).CreateTable(&orm.CreateTableOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func createFakeUsers(db *pg.DB, count int) error {
+	err := db.Model(&User{}).Where("name = ?", "fake-0").Select()
+	if err == nil {
+		log.Printf("users already exists")
+		return nil
+	}
+	users := make([]User, count)
+	for i := 0; i < count; i++ {
+		users[i] = User{
+			Name:   fmt.Sprintf("fake-%d", i),
+			Emails: []string{fmt.Sprintf("fake-master-%d@email.com", i), fmt.Sprintf("fake-slave-%d@email.com", i)},
+		}
+	}
+
+	_, err = db.Model(&users).Insert()
+	return err
 }
